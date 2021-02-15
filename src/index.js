69dc7e607ca0a5074c699b10efd7b22861883a05
ranjith473 <74918552+ranjith473@users.noreply.github.com>
2021-02-15 22:06:31 +0530

    code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-const onePageArticleCount = 10
+const onePageArticleCount = 10;
 
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const isInvalid = (val) =>{
+  return val === null || val === undefined|| isNaN(Number(val));
+};
 
+app.get("/newFeeds", async (req,res)=>{
+  const { limit, offset } = req.query;
+
+const lim = limit == undefined ? 10 : isNaN(limit) ? 10 : Number(limit);
+const off = offset == undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
+const result = await newsArticleModel.find().skip(off).limit(lim);
+res.send(result);
+
+  
+
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
