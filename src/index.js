0bf1bde5f3fd4ea586cf3b2896f8b195a7e1c53f
ranjith <ranjith.juluru14@gmail.com>
2021-03-01 22:17:03 +0530

    code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const { newsArticleModel } = require("./connector");
+// const mongoose = require("mongoose");
+const app = express();
+const port = 8080;
 
 const onePageArticleCount = 10;
-
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const isInvalid = (val) =>{
-  return val === null || val === undefined|| isNaN(Number(val));
-};
-
-app.get("/newFeeds", async (req,res)=>{
+app.get("/newFeeds", async (req, res) => {
   const { limit, offset } = req.query;
 
-const lim = limit == undefined ? 10 : isNaN(limit) ? 10 : Number(limit);
-const off = offset == undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
-const result = await newsArticleModel.find().skip(off).limit(lim);
-res.send(result);
+  const lim = limit == undefined ? 10 : isNaN(limit) ? 10 : Number(limit);
+  const off = offset == undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
+  const result = await newsArticleModel.find().skip(off).limit(lim);
+  res.send(result);
+});
 
-  
-
-})
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
